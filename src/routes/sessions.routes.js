60f65e1d5b4537271f19308e5cbf824e1430dbc8
felipe <enggazolla@gmail.com>
2024-09-05 16:30:10 -0300

    first commit

---
+++ src/routes/sessions.routes.js
@@ -0,0 +1,11 @@
+// Router Import
+const { Router } = require('express');
+
+const SessionsController = require("../controllers/SessionsController");
+const sessionsController = new SessionsController();
+
+const sessionsRoutes = Router();
+
+sessionsRoutes.post("/", sessionsController.create);
+
+module.exports = sessionsRoutes;
